378596ea1f3c03ad2e3a1934c90cd6717ac4930c
Valtech-Obelix <olaf.henke@valtech-mobility.com>
2026-02-22 13:31:38 +0100

    feat(logging): add error logging with stacktrace in service layer

--- src/services/incident_analyst_service.py
+++ src/services/incident_analyst_service.py
@@ -1,8 +1,7 @@
 # incident_analyst_service.py
 import   logging
 from     src.domain.incident_analyst                       import IncidentAnalyst
-
-
+d
 class IncidentAnalystService:
 
     def __init__(self, p_repository):
@@ -25,35 +24,35 @@ class IncidentAnalystService:
                            , p_ende_datum
                         )
                                 
-
-        analyst = IncidentAnalyst(  p_id=None,
+        try:
+            analyst = IncidentAnalyst(  p_id=None,
                                   , p_vornamen    = p_vornamen
                                   , p_nachname    = p_nachname
                                   , p_email       = p_email
                                   , p_start_datum = p_start_datum
                                   , p_ende_datum  = p_ende_datum
                                  )
-
-        self._logger.info(
-            "Creating IncidentAnalyst: %s, %s",
-            p_nachname,
-            p_vornamen
-        )
-
-        return self._repository.add(analyst)
+            result  = self._repository.add(analyst)
+            self._logger.info(  "Creating IncidentAnalyst: %s, %s"
+                              , p_nachname
+                              , p_vornamen
+                             )
+        except Exception as e:
+            self._logger.error("Error while creating IncidentAnalyst: %s", str(e), exc_info=True)
+            raise
 
     # Ref: UC-002
     def delete(self, p_id: int):
         self._logger.debug("Deleting IncidentAnalyst with id=%s", p_id)
 
-        self._repository.delete(p_id)
+        try:
+            self._repository.delete(p_id)
 
-        self._logger.info(
-            "Deleting IncidentAnalyst with id=%s",
-            p_id
-        )
-
-        self._logger.debug("Delete operation executed")
+            self._logger.info("Deleting IncidentAnalyst with id=%s", p_id)
+            self._logger.debug("Delete operation executed")
+        except Exception as e:
+            self._logger.error("Error while deleting IncidentAnalyst: %s", str(e), exc_info=True)
+            raise
 
 
     def get_all(self):
