32c6efe5f9afa8c893c600e49a821fb96a197b3a
Valtech-Obelix <olaf.henke@valtech-mobility.com>
2026-02-22 13:00:48 +0100

    refactor(logging): add logging to IncidentAnalystService

--- src/services/incident_analyst_service.py
+++ src/services/incident_analyst_service.py
@@ -1,11 +1,12 @@
 # incident_analyst_service.py
-
-from src.domain.incident_analyst import IncidentAnalyst
+import   logging
+from     src.domain.incident_analyst                       import IncidentAnalyst
 
 
 class IncidentAnalystService:
 
     def __init__(self, p_repository):
+        self._logger = logging.getLogger(__name__)
         self._repository = p_repository
 
     # Ref: UC-001
@@ -27,11 +28,23 @@ class IncidentAnalystService:
             p_ende_datum=p_ende_datum
         )
 
+        self._logger.info(
+            "Creating IncidentAnalyst: %s, %s",
+            p_nachname,
+            p_vornamen
+        )
+
         return self._repository.add(analyst)
 
     # Ref: UC-002
     def delete(self, p_id: int):
         self._repository.delete(p_id)
+
+        self._logger.info(
+            "Deleting IncidentAnalyst with id=%s",
+            p_id
+        )
+
 
     def get_all(self):
         return self._repository.get_all()
